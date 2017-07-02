aa59ac94f0d8110f06ea336f8916c11b6408d845
Stefan Joesch <stefan.joesch@joesch-it.de>
2017-07-02 19:08:49 +0200

    examples added

--- ServiceTest/app/src/main/java/de/joesch_it/servicetest/SampleAlarmReceiver.java
+++ ServiceTest/app/src/main/java/de/joesch_it/servicetest/SampleAlarmReceiver.java
@@ -45,44 +45,9 @@ public class SampleAlarmReceiver extends WakefulBroadcastReceiver {
 
         Calendar calendar = Calendar.getInstance();
         calendar.setTimeInMillis(System.currentTimeMillis());
-        // Set the alarm's trigger time to 8:30 a.m.
-        //calendar.set(Calendar.HOUR_OF_DAY, 8);
-        //calendar.set(Calendar.MINUTE, 30);
-  
-        /* 
-         * If you don't have precise time requirements, use an inexact repeating alarm
-         * the minimize the drain on the device battery.
-         * 
-         * The call below specifies the alarm type, the trigger time, the interval at
-         * which the alarm is fired, and the alarm's associated PendingIntent.
-         * It uses the alarm type RTC_WAKEUP ("Real Time Clock" wake up), which wakes up 
-         * the device and triggers the alarm according to the time of the device's clock. 
-         * 
-         * Alternatively, you can use the alarm type ELAPSED_REALTIME_WAKEUP to trigger 
-         * an alarm based on how much time has elapsed since the device was booted. This 
-         * is the preferred choice if your alarm is based on elapsed time--for example, if 
-         * you simply want your alarm to fire every 60 minutes. You only need to use 
-         * RTC_WAKEUP if you want your alarm to fire at a particular date/time. Remember 
-         * that clock-based time may not translate well to other locales, and that your 
-         * app's behavior could be affected by the user changing the device's time setting.
-         * 
-         * Here are some examples of ELAPSED_REALTIME_WAKEUP:
-         * 
-         * // Wake up the device to fire a one-time alarm in one minute.
-         * mAlarmManager.set(AlarmManager.ELAPSED_REALTIME_WAKEUP, SystemClock.elapsedRealtime() + 60*1000, mPendingIntent);
-         *        
-         * // Wake up the device to fire the alarm in 30 minutes, and every 30 minutes
-         * // after that.
-         * mAlarmManager.setInexactRepeating(AlarmManager.ELAPSED_REALTIME_WAKEUP, AlarmManager.INTERVAL_HALF_HOUR, AlarmManager.INTERVAL_HALF_HOUR, mPendingIntent);
-         */
-        
-        // Set the alarm to fire at approximately 8:30 a.m., according to the device's
-        // clock, and to repeat once a day.
-        //mAlarmManager.setInexactRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), AlarmManager.INTERVAL_DAY, mPendingIntent);
+
         mAlarmManager.setInexactRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), 60000, mPendingIntent);
 
-        // Enable {@code SampleBootReceiver} to automatically restart the alarm when the
-        // device is rebooted.
         ComponentName receiver = new ComponentName(context, SampleBootReceiver.class);
         PackageManager pm = context.getPackageManager();
 
@@ -90,18 +55,13 @@ public class SampleAlarmReceiver extends WakefulBroadcastReceiver {
     }
 
     public void cancelAlarm(Context context) {
-        // If the alarm has been set, cancel it.
         if (mAlarmManager != null) {
             mAlarmManager.cancel(mPendingIntent);
         }
         
-        // Disable {@code SampleBootReceiver} so that it doesn't automatically restart the 
-        // alarm when the device is rebooted.
         ComponentName receiver = new ComponentName(context, SampleBootReceiver.class);
         PackageManager pm = context.getPackageManager();
 
-        pm.setComponentEnabledSetting(receiver,
-                PackageManager.COMPONENT_ENABLED_STATE_DISABLED,
-                PackageManager.DONT_KILL_APP);
+        pm.setComponentEnabledSetting(receiver, PackageManager.COMPONENT_ENABLED_STATE_DISABLED, PackageManager.DONT_KILL_APP);
     }
 }
--- WidgetUpdaterTest/app/src/main/java/de/joesch_it/widgetupdatertest/ExampleAppWidgetProvider.java
+++ WidgetUpdaterTest/app/src/main/java/de/joesch_it/widgetupdatertest/ExampleAppWidgetProvider.java
@@ -4,6 +4,7 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.Locale;
 
 import android.app.AlarmManager;
 import android.app.PendingIntent;
@@ -16,38 +17,33 @@ import android.util.Log;
 import android.widget.RemoteViews;
 
 public class ExampleAppWidgetProvider extends AppWidgetProvider {
+
 	private static final String LOG_TAG = "ExampleWidget";
 
-	private static final DateFormat df = new SimpleDateFormat("hh:mm:ss");
+	private static final DateFormat df = new SimpleDateFormat("HH:mm:ss", Locale.getDefault());
 
-	/**
-	 * Custom Intent name that is used by the AlarmManager to tell us to update the clock once per second.
-	 */
 	public static String CLOCK_WIDGET_UPDATE = "com.eightbitcloud.example.widget.8BITCLOCK_WIDGET_UPDATE";
+	public static String BOOT_COMPLETED = "android.intent.action.BOOT_COMPLETED";
 
 	@Override
 	public void onReceive(Context context, Intent intent) {
 		super.onReceive(context, intent);
 		
-		if (CLOCK_WIDGET_UPDATE.equals(intent.getAction())) {
+		if (intent.getAction().equals(CLOCK_WIDGET_UPDATE) || intent.getAction().equals(BOOT_COMPLETED)) {
+
 			Log.d(LOG_TAG, "Clock update");
-			// Get the widget manager and ids for this widget provider, then call the shared
-			// clock update method.
 			ComponentName thisAppWidget = new ComponentName(context.getPackageName(), getClass().getName());
 		    AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
 		    int ids[] = appWidgetManager.getAppWidgetIds(thisAppWidget);
 		    for (int appWidgetID: ids) {
 				updateAppWidget(context, appWidgetManager, appWidgetID);
-		    	
 		    }
 		}
 	}
-	
+
 	private PendingIntent createClockTickIntent(Context context) {
         Intent intent = new Intent(CLOCK_WIDGET_UPDATE);
-        PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
-
-        return pendingIntent;
+		return PendingIntent.getBroadcast(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
 	}
 	
 	@Override
@@ -66,36 +62,25 @@ public class ExampleAppWidgetProvider extends AppWidgetProvider {
     	
         Calendar calendar = Calendar.getInstance();
         calendar.setTimeInMillis(System.currentTimeMillis());
-        calendar.add(Calendar.SECOND, 1);
-        alarmManager.setRepeating(AlarmManager.RTC, calendar.getTimeInMillis(), 1000, createClockTickIntent(context));
+        //calendar.add(Calendar.MINUTE, 1);
+        alarmManager.setInexactRepeating(AlarmManager.RTC, calendar.getTimeInMillis(), 60000, createClockTickIntent(context));
 	}
 	
 	
 	@Override
 	public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
-		final int N = appWidgetIds.length;
 
 		Log.d(LOG_TAG, "Updating Example Widgets.");
 
-		// Perform this loop procedure for each App Widget that belongs to this
-		// provider
-		for (int i = 0; i < N; i++) {
-			int appWidgetId = appWidgetIds[i];
+		for (int appWidgetId : appWidgetIds) {
 
-			// Create an Intent to launch ExampleActivity
 			Intent intent = new Intent(context, WidgetExampleActivity.class);
-			PendingIntent pendingIntent = PendingIntent.getActivity(context, 0,	intent, 0);
+			PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
 
-			// Get the layout for the App Widget and attach an on-click listener
-			// to the button
 			RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget1);
 			views.setOnClickPendingIntent(R.id.button, pendingIntent);
-			// Tell the AppWidgetManager to perform an update on the current app
-			// widget
 			appWidgetManager.updateAppWidget(appWidgetId, views);
 
-
-			// Update The clock label using a shared method
 			updateAppWidget(context, appWidgetManager, appWidgetId);
 		}
 	}
